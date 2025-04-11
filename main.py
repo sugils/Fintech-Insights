7c5704093e3223cf69a3bb7f5645d4912ef5cb24
Sugil <109132770+sugils@users.noreply.github.com>
2025-04-12 00:31:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ def run_daily_job():
     try:
          # Step 1: Fetch data from CoinGecko
         coin_list = ["bitcoin", "ethereum", "solana", "cardano", "ripple"]
-        failed_coins = []
         for coin in coin_list:
             data = fetch_coingecko_data(coin)
             if data:
                 # Step 2: Store raw data in DB
                 store_data(data)
             else:
-                failed_coins.append(coin)
                 print(f"Skipping {coin}: Data fetch failed.")
 
         # Step 3: Process all records and generate AI insights
@@ -25,8 +23,7 @@ def run_daily_job():
         return jsonify({
             "status": "success",
             "message": "Job completed and insights generated.",
-            "insights_generated":insights,
-            "failed_coins": failed_coins
+            "insights_generated":insights
         }), 200
 
     except Exception as e:
