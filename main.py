51f1772172f17f54d30a8fa638d5b726acb89938
sugil <sugilsanthosh243@gmail.com>
2025-04-11 20:47:23 +0530

    Configured host and port for Railway deployment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controller import insights_blueprint
 
@@ -5,4 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(insights_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the dynamic port from Railway
+    app.run(debug=False, host="0.0.0.0", port=port)  # Bind to all interfaces
